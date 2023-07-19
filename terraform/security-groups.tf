ceb4973c5292a9d51c107b325c5beab33d4707d8
John Flynn <JJimmyFlynn@gmail.com>
2023-07-18 23:51:03 -0400

    Tweak Security Groups

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -46,8 +46,8 @@ resource "aws_vpc_security_group_ingress_rule" "ecs_allow_lb_in" {
   security_group_id            = aws_security_group.ecs_service_sg.id
   referenced_security_group_id = aws_security_group.load_balancer_sg.id
   ip_protocol                  = "tcp"
-  from_port                    = 0
-  to_port                      = 65535
+  from_port                    = 8080
+  to_port                      = 8080
   description                  = "Allow ALB inbound connections"
 }
 
@@ -66,7 +66,7 @@ resource "aws_vpc_security_group_egress_rule" "ecs_allow_all_out" {
 resource "aws_security_group" "allow_ecs" {
   name        = "allow_ecs"
   vpc_id      = aws_vpc.craft_vpc.id
-  description = "Allows inbound originated traffic from the ECS Service security group"
+  description = "Allows inbound traffic from the ECS Service security group"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ecs_inbound" {
